e85e3738d22a24fb29a29a2b76b165d53c6a03d8
James Ridgway <james@jamesridgway.co.uk>
2021-10-23 20:42:00 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+
+from strip_manager import StripManager
+
+app = Flask(__name__)
+strip_manager = StripManager.default()
+
+
+@app.route("/led-strip")
+def status():
+    return jsonify(strip_manager.status())
+
+
+@app.route("/led-strip", methods=['POST'])
+def turn_on():
+    strip_manager.solid_color(255, 64, 0)
+    return jsonify(strip_manager.status())
+
+
+@app.route("/led-strip", methods=['DELETE'])
+def turn_off():
+    strip_manager.clear()
+    return jsonify(strip_manager.status())
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ strip_manager.py
@@ -0,0 +1,53 @@
+import time
+
+from rpi_ws281x import Adafruit_NeoPixel, Color
+
+
+class StripManager:
+
+    @staticmethod
+    def default():
+        led_count = 159  # Number of LED pixels.
+        led_pin = 18  # GPIO pin connected to the pixels (18 uses PWM!).
+        led_freq_hz = 800000  # LED signal frequency in hertz (usually 800khz)
+        led_dma = 10  # DMA channel to use for generating signal (try 10)
+        led_brightness = 255  # Set to 0 for darkest and 255 for brightest
+        led_invert = False  # True to invert the signal (when using NPN transistor level shift)
+        led_channel = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
+        return StripManager(led_count, led_pin, led_freq_hz, led_dma, led_invert, led_brightness, led_channel)
+
+    def __init__(self, led_count, led_pin, led_freq_hz, led_dma, led_invert, led_brightness, led_channel):
+        self.strip = Adafruit_NeoPixel(led_count, led_pin, led_freq_hz, led_dma, led_invert, led_brightness,
+                                       led_channel)
+        self.strip.begin()
+        self.state = 'off'
+
+    def solid_color(self, r, g, b):
+        self.strip.setBrightness(255)
+        for i in range(0, self.strip.numPixels()):
+            self.strip.setPixelColor(i, Color(r, g, b))
+            self.strip.show()
+        self.state = 'on'
+
+    def alert(self, r, g, b, wait_ms=50, iterations=10):
+        for j in range(iterations):
+            for q in range(3):
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i + q, Color(r, g, b))
+                self.strip.show()
+                time.sleep(wait_ms / 1000.0)
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i + q, 0)
+        self.state = 'on'
+
+    def clear(self):
+        self.solid_color(0, 0, 0)
+        self.state = 'off'
+
+    def orange(self):
+        self.solid_color(255, 64, 0)
+
+    def status(self):
+        return {
+            'state': self.state
+        }
